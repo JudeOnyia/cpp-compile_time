f82e1dc817588636e0b5a8d951a623cba0fcbe66
Jude Onyia <judeonyia10@gmail.com>
2020-05-30 00:53:50 -0700

    1) Wrote max size, size and capacity functions 2) Wrote data, begin and end functions 3) Wrote operator[] overload

--- app/test_cexpr_basic_string.cpp
+++ app/test_cexpr_basic_string.cpp
@@ -1,14 +1,45 @@
 #include "ra/cexpr_basic_string.hpp"
 #include <iostream>
+#include <cstddef>
 
 int main(){
 
 	using std::cout;
 	using std::endl;
-	ra::cexpr::cexpr_basic_string<char,10> obj_A();
-	char s[] = {'j','u','d','e'};
-	ra::cexpr::cexpr_basic_string<char,6> obj_B(s);
-	//cout << "obj_B = " << obj_B[0] << obj_B[1] << obj_B[2] << obj_B[3] << endl; 
+	constexpr ra::cexpr::cexpr_basic_string<char,10> obj_A();
+	const char s[] = {'j','u','d','e','\0'};
+	constexpr ra::cexpr::cexpr_basic_string<char,6> obj_B(s);
+	cout << "obj_B = " << obj_B[0] << obj_B[1] << obj_B[2] << obj_B[3] << endl;
+	cout << "max size: " << obj_B.max_size() << endl;
+	cout << "capacity: " << obj_B.capacity() << endl;
+	cout << "size: " << obj_B.size() << endl;
+	const unsigned char j[] = {'o', 'n', 'y', 'i', 'a', '\0'};
+	//constexpr const unsigned char* j_first = &(j[0]);
+	//constexpr const unsigned char* j_last = &(j[5]);
+	constexpr ra::cexpr::cexpr_basic_string<unsigned char,6> obj_C(&j[0], &j[5]);
+	const unsigned char* t = obj_C.data();
+	for(std::size_t i=0; i<obj_C.size(); ++i){
+		cout<< *t;
+		++t;
+	}
+	cout << endl;
+	ra::cexpr::cexpr_basic_string<unsigned char,6> obj_D(obj_C);
+	unsigned char* tt = obj_D.data();
+	for(std::size_t i=0; i<=obj_D.size(); ++i){
+		cout<< *tt;
+		++tt;
+	}
+	cout << endl;
+	tt = obj_D.end();
+	*tt = 'w';
+	--tt;
+	*tt = 'g';
+	tt = obj_D.begin();
+	for(std::size_t i=0; i<=obj_D.size(); ++i){
+		cout<< *tt;
+		++tt;
+	}
+	cout << endl;
 	return 0;
 }
 
--- include/ra/cexpr_basic_string.hpp
+++ include/ra/cexpr_basic_string.hpp
@@ -58,6 +58,83 @@ namespace ra::cexpr{
 				}
 			}
 
+			// Creates a string with the contents specified by the characters
+			// in the iterator range [first, last).
+			// If the string does not have sufficient capacity to hold
+			// the character data provided, an exception of type
+			// std::runtime_error is thrown.
+			constexpr cexpr_basic_string(const_iterator first, const_iterator last) : charArray_{0}, charSize_(0){
+				if(M < (last - first)){
+					throw std::runtime_error("String does not have sufficient capacity");
+				}
+				for(const_iterator i=first; i < last; ++i){
+					charArray_[charSize_] = *i;
+					++charSize_;
+				}
+			}
+
+			// Returns the maximum number of characters that can be held by a
+			// string of this type.
+			// The value returned is the template parameter M
+			static constexpr size_type max_size() { return M; }
+
+			// Returns the maximum number of characters that the string can
+			// hold. The value returned is always the template parameter M
+			constexpr size_type capacity() const { return M; }
+
+			// Returns the number of characters in the string (excluding the
+			// dummy null character)
+			constexpr size_type size() const { return charSize_; }
+
+			// Returns a pointer to the first character in the string
+			// The pointer that is returned is guaranteed to point to a
+			// null-terminated character array
+			// The user of this class shall not alter the dummy null
+			// character stored at data() + size().
+			value_type* data(){
+				return charArray_;
+			}
+			const value_type* data() const {
+				return charArray_;
+			}
+
+			// Returns an iterator referring to the first character in the
+			// string
+			constexpr iterator begin() {
+				return charArray_;
+			}
+			constexpr const_iterator begin() const {
+				return charArray_;
+			}
+
+			// Returns an iterator referring to the fictitious
+			// one-past-the-end character in the string
+			constexpr iterator end(){
+				return (&charArray_[charSize_]);
+			}
+			constexpr const_iterator end() const {
+				return (&charArray_[charSize_]);
+			}
+
+			// Returns a reference to the i-th character in the string if i
+			// is less than the string size; and returns a reference to the
+			// dummy null character if i equals the string size.
+			// Precondition: The index i is such that i >= 0 and i <= size().
+			constexpr reference operator[](size_type i){
+				if(i<0 || i>charSize_){
+					throw std::domain_error("invalid array access");
+				}
+				if(i==charSize_){ return nullChar_; }
+				else{ return charArray_[i];}
+			}
+			constexpr const_reference operator[](size_type i)const{
+				if(i<0 || i>charSize_){
+					throw std::domain_error("invalid array access");
+				}
+				if(i==charSize_){ return nullChar_; }
+				else{ return charArray_[i];}
+			}
+
 		private:
 			value_type charArray_[M+1];
 			size_type charSize_;
